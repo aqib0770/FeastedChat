7f14d88bd74313b9c86bb2f609b06541ee41c785
Aqib Ansari <aqibansari72a@gmail.com>
2026-08-08 01:27:10 +0530

    fix: refactor useIsMobile hook for improved mobile detection logic

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -3,15 +3,19 @@ import * as React from 'react';
 const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const subscribe = React.useCallback((callback: () => void) => {
+  const [isMobile, setIsMobile] = React.useState<boolean>(() => {
+    if (typeof window === 'undefined') return false;
+    return window.innerWidth < MOBILE_BREAKPOINT;
+  });
+
+  React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
-    mql.addEventListener('change', callback);
-    return () => mql.removeEventListener('change', callback);
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+    mql.addEventListener('change', onChange);
+    return () => mql.removeEventListener('change', onChange);
   }, []);
 
-  return React.useSyncExternalStore(
-    subscribe,
-    () => window.innerWidth < MOBILE_BREAKPOINT,
-    () => false
-  );
+  return !!isMobile;
 }
